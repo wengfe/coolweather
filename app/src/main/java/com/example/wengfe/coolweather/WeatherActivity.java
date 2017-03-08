fd29f8b89989fbf94b3f311a96e65a18cebb8c24
WengFe <214952026@qq.com>
2017-03-08 23:50:16 +0800

    增加后台自动更新功能

--- app/src/main/java/com/example/wengfe/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/wengfe/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.wengfe.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.wengfe.coolweather.gson.Forecast;
 import com.example.wengfe.coolweather.gson.Weather;
+import com.example.wengfe.coolweather.service.AutoUpdateService;
 import com.example.wengfe.coolweather.util.HttpUtil;
 import com.example.wengfe.coolweather.util.Utility;
 
@@ -217,5 +219,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
